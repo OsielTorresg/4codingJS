01950bb03ffad7757a23694ce106fcb8399dd0e2
osiel torres gonzalez <osieltorresgonzalez@R2Hs-MacBook-Pro.local>
2022-02-01 12:13:50 -0500

    changes

--- script.js
+++ script.js
@@ -11,12 +11,13 @@ console.log(avg([1, 4, 7]));
 console.log(avg([10, 5]));
 console.log(avg([1.5, 3, 2.5, 1]));
 /////medium/////
+let numbers = [4, 5, 6, 7, 0, 1, 2];
+let firstNums = (numArr, target) => numArr.indexOf(target);
+console.log(firstNums(numbers, 7));
 
-
-
-const num = [4, 5, 6, 7, 0, 1, 2];
-const idx = num.indexOf(0);
-const idx2 = num.indexOf(3);
-console.log(idx);
-console.log(idx2);
+// const num = [4, 5, 6, 7, 0, 1, 2];
+// const idx = num.indexOf(0);
+// const idx2 = num.indexOf(3);
+// console.log(idx);
+// console.log(idx2);
 /////hard/////
